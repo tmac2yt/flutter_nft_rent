c56e05e8b4dd6260b5781e68ae591463a58098ca
hezhenghai <15088518315@139.com>
2020-08-21 09:14:51 +0800

    fix #modify net framework

--- lib/utils/net_utils.dart
+++ lib/utils/net_utils.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 class NetUtils {
+  static Dio dio = Dio();
+
   static Future get(String url, Map<String, dynamic> params) async {
     if (url != null && params != null && params.isNotEmpty) {
       //拼接参数
@@ -12,12 +15,23 @@ class NetUtils {
       url += paramStr;
     }
     print('NetUtils:$url');
-    http.Response response = await http.get(url);
-    return response.body;
+    try {
+      Response response = await dio.get(url);
+      print(response);
+      return response.data;
+    } catch (e) {
+      print(e);
+    }
+    return null;
   }
 
   static Future post(String url, Map<String, dynamic> params) async {
-    http.Response response = await http.post(url, body: params);
-    return response.body;
+    try {
+      Response response = await dio.post(url, data: params);
+      return response.data;
+    }catch(e){
+      print(e);
+    }
+    return null;
   }
 }
